416e720e5d24ba97d48ec74ae9cc5ba64b4a5967
Przedtop <lukasztoma12@gmail.com>
2025-03-10 16:56:58 +0100

    Files update

--- src/main/java/com/przedtop/bank/system/controllers/model/MoneyTransferRequestDataModel.java
+++ src/main/java/com/przedtop/bank/system/controllers/model/MoneyTransferRequestDataModel.java
@@ -2,18 +2,26 @@ package com.przedtop.bank.system.controllers.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 public class MoneyTransferRequestDataModel {
 
     @JsonAlias({"sender", "senderId", "from"})
     @JsonProperty("sender")
+    @NotNull(message = "Sender account number is required")
     private Long senderAccountNumber;
+
     @JsonAlias({"receiver", "receiverId", "to"})
     @JsonProperty("receiver")
+    @NotNull(message = "Receiver account number is required")
     private Long receiverAccountNumber;
+
     @JsonAlias({"value"})
     @JsonProperty("amount")
-    private double amountToTransfer;
+    @NotNull(message = "Transfer amount is required")
+    @Min(value = 1, message = "Minimum transfer amount is 1")
+    private Double amountToTransfer;
 
     @Override
     public String toString() {
@@ -28,13 +36,11 @@ public class MoneyTransferRequestDataModel {
         return senderAccountNumber;
     }
 
-    public double getAmountToTransfer() {
-        return amountToTransfer;
-    }
-
     public Long getReceiverAccountNumber() {
         return receiverAccountNumber;
     }
 
-
+    public Double getAmountToTransfer() {
+        return amountToTransfer;
+    }
 }
