d95c410fc3473dea325fe9eb484e6bad374213db
Przedtop <156699756+Przedtop@users.noreply.github.com>
2025-03-12 15:11:30 +0100

    Add files via upload

--- resources/systembankowy.sql
+++ resources/systembankowy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2025 at 06:45 PM
+-- Generation Time: Mar 12, 2025 at 03:11 PM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -40,16 +40,16 @@ CREATE TABLE `accounts` (
 --
 
 INSERT INTO `accounts` (`id`, `account_number`, `balance`, `create_date`, `user_id`) VALUES
+(0, 0, 0, '', 0),
 (2, 9341124, 2123, 'accountRequestDataModel.getData_zalozenia()', 5),
 (4, 1241, 8000, 'asfd', 7),
 (5, 1241, 8000, 'asfd', 7),
-(6, 100, 7500, 'asfd', 7),
-(7, 101, 8500, 'asfd', 7),
+(6, 100, 7595.8, 'asfd', 7),
+(7, 101, 9105.000000000004, 'asfd', 7),
 (8, 681009124, 0, '1000:10:2025', 12),
-(10, 10, 400, '100-100', 14),
-(11, 10, 400, '100-100', 14),
-(12, 10, 400, '100-100', 14),
-(13, 10, 400, '100-100', 14);
+(10, 10, 100, '100-100', 14),
+(14, 223816382, 1000, '2025-03-07 17:56', 0),
+(15, 824687255, 1000, '2025-03-12 13:05', 0);
 
 -- --------------------------------------------------------
 
@@ -60,8 +60,8 @@ INSERT INTO `accounts` (`id`, `account_number`, `balance`, `create_date`, `user_
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
-  `lastName` varchar(31) NOT NULL,
-  `identificationNumber` int(11) NOT NULL,
+  `last_name` varchar(31) NOT NULL,
+  `identification_number` int(11) NOT NULL,
   `login` varchar(24) NOT NULL,
   `password` varchar(24) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -70,8 +70,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `lastName`, `identificationNumber`, `login`, `password`) VALUES
-(1, 'dasd', 'asd', 1234, 'skibi', 'Asss'),
+INSERT INTO `users` (`id`, `name`, `last_name`, `identification_number`, `login`, `password`) VALUES
+(1, '123tal', 'nie', 123451, 'klgao', 'juasd'),
 (3, 'Imie', 'Nazwis', 123124, 'Login', 'Haslo'),
 (4, 'Imie', 'Nazwis', 123124, 'Login', 'Haslo'),
 (5, 'Jan', 'Kowalski', 123444, 'janek', 'password123'),
@@ -79,17 +79,36 @@ INSERT INTO `users` (`id`, `name`, `lastName`, `identificationNumber`, `login`, 
 (7, 'asd', 'asdqa', 1190123, 'login', 'haslo'),
 (8, 'asd', 'asdqa', 1190123, 'login', 'haslo1'),
 (9, 'Jan', 'Kowalski', 0, 'janek', 'password123'),
-(10, 'Jan', 'Kowalski', 9090123, 'janek', 'password123'),
+(10, 'azsd', 'asd1', 12301, 'niema123mloginu', 'skibi123di'),
 (11, 'Jan', 'Kowalski', 9090123, 'janek', 'password123'),
-(12, 'Jan', 'Kowalski', 0, 'janek', 'password123'),
-(13, 'Jan', 'Kowalski', 0, 'janek', 'password123'),
 (14, 'Jan', 'Kowalski', 123444, 'janek', 'password123'),
 (15, 'Jan', 'Kowalski', 123444, 'janek', 'password123'),
 (16, 'Jan', 'Kowalski', 123444, 'janek', 'password123'),
 (17, 'Jan', 'Kowalski', 0, 'janek', 'mojehaslo'),
 (18, 'Jan', 'Kowalski', 99, 'janek', 'mojehaslo'),
-(19, 'skibid', 'asfd', 100000, 'logjmn', 'asdhaslo'),
-(20, 'lebisko', 'leb', 12351, 'asd', 'haselisko');
+(19, '123tal', 'nie', 123451, 'klgao', 'juasd'),
+(23, '123tal', 'nie', 123451, 'klgao', 'juasd'),
+(24, '123tal', 'nie', 123451, 'klgao', 'juasd'),
+(25, '123tal', 'nie', 123451, 'klgao', 'juasd'),
+(26, '123tal', 'nie', 123451, 'klgao', 'juasd'),
+(27, '123', '123', 123, '123', '123'),
+(28, '123asd', '123', 123, '123', '123'),
+(29, '123asd', '123', 123, '123', '123'),
+(30, '', '123', 123, '123', '123'),
+(31, '', '12331', 121233, '123123', '121233'),
+(32, 'siema asd', '12331', 121233, '123123', '121233'),
+(33, '', '12331', 121233, '123123', '121233'),
+(34, '', '123', 121233, '123123', '121233'),
+(35, '', '123', 123, '123123', '121233'),
+(36, '', '123', 12, '123123', '121233'),
+(37, 'azsd', 'asd1', 1, 'niemamloginu', 'skibidi'),
+(38, 'azsd', 'asd1', 1, 'niemamloginu', 'skibidi'),
+(39, 'azsd', 'asd1', 1, 'niemamloginu', 'skibidi'),
+(40, 'azsd', 'asd1', 1, 'niemamloginu', 'skibidi'),
+(41, 'azsd', 'asd1', 1, 'niemamloginu', 'skibidi'),
+(42, 'azsd', 'asd1', 1, 'niemamloginu', 'skibidi'),
+(43, 'azsd', 'asd1', 1, 'niemamloginu', 'skibidi'),
+(44, 'azsd', 'asd1', 12301, 'niema123mloginu', 'skibi123di');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -115,13 +134,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
