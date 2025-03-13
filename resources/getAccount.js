bf0de954160f03074ecacaa45faf274c6d7a9bf2
Przedtop <156699756+przedtop@users.noreply.github.com>
2025-03-13 10:20:06 +0100

    Get Account website and script changes

--- resources/getAccount.js
+++ resources/getAccount.js
@@ -1,30 +1,7 @@
-// Handle form submission using Fetch API
-function getAccount(){
+async function getAccountById() {
 
-    const senderAccountNumber = document.getElementById('senderAccountNumber').value;
-    const receiverAccountNumber = document.getElementById('receiverAccountNumber').value;
-
-    // Create the request body object
-    const transferData = {
-        senderAccountNumber: senderAccountNumber,
-        receiverAccountNumber: receiverAccountNumber,
-    };
-
-    // Send the POST request using Fetch API
-    fetch('http://localhost:8080/api/transfer', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(transferData)
-    })
-        .then(response => response.text())
-        .then(data => {
-            console.log('Sukces:', data);
-            document.getElementById("response").innerText = data;
-        })
-        .catch((error) => {
-            console.error('Błąd:', error);
-            document.getElementById("response").innerText = error;
-        });
+    const accountId = document.getElementById('accountId').value;
+    const response = await fetch(`/api/accounts/${accountId}`);
+    const data = await response.json();
+    document.getElementById("response").innerText = JSON.stringify(data, null, 2);
 }
