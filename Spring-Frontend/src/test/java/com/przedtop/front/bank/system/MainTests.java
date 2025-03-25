c6096fb4f380dcd94fb677847a82536ab1ad963b
Przedtop <156699756+przedtop@users.noreply.github.com>
2025-03-25 13:40:35 +0100

    Refactor project structure: rename Java files and resources for improved organization and clarity

---
+++ Spring-Frontend/src/test/java/com/przedtop/front/bank/system/MainTests.java
@@ -0,0 +1,13 @@
+package com.przedtop.front.bank.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
